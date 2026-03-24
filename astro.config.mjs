1fe76a1ae8a53e3c97d68d6d135b4f72c1783f97
Mike Repec <mikerepec@pm.me>
2026-03-24 09:43:43 +0100

    Add service worker with astrojs-service-worker (Workbox)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
+import serviceWorker from 'astrojs-service-worker'
 
 export default defineConfig({
   output: 'static',
+  integrations: [serviceWorker()],
 })
